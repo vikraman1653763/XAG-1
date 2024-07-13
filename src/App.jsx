963b5b3da3a2a7e75823d74253ace375681ee263
vikraman1653763 <vikraman1653763@gmail.com>
2024-07-13 11:53:39 +0530

    13-07-2024

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
+import ScrollToTop from './components/ScrollToTop';
+
 import Home from '/src/home.jsx';
 import Contact from '/src/contact.jsx';
 import About from '/src/about.jsx';
@@ -18,28 +20,29 @@ import Career from './career/Career.jsx';
 const App = () => {
   return (
     <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home/>} />
-         <Route path="/about" element={<About/>} /> 
-        <Route path="/contact" element={<Contact/>} /> 
-        <Route path="/agr16" element={<AGR16 />} />
-        <Route path="/agr10" element={<AGR16 />} />
-        <Route path="/Batteries" element={<Battery />} />
-        <Route path="/battery/:title" element={<BatteryDetails />} />
-        <Route path="/flight-controller" element={<FlightController />} />
-        <Route path="/training" element={<Training />} />
-        <Route path="/Blog" element={<Blog />} />
-        <Route path='/Blog/:title' element={<BlogDetails/>}/>
-        <Route path="/career" element={<Career />} />
+          <ScrollToTop/>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home/>} />
+            <Route path="/about" element={<About/>} /> 
+            <Route path="/contact" element={<Contact/>} /> 
+            <Route path="/agr16" element={<AGR16 />} />
+            <Route path="/agr10" element={<AGR16 />} />
+            <Route path="/Batteries" element={<Battery />} />
+            <Route path="/battery/:title" element={<BatteryDetails />} />
+            <Route path="/flight-controller" element={<FlightController />} />
+            <Route path="/training" element={<Training />} />
+            <Route path="/Blog" element={<Blog />} />
+            <Route path='/Blog/:title' element={<BlogDetails/>}/>
+            <Route path="/career" element={<Career />} />
 
-        
-      </Routes>
-      <Footer />
-      <button className="rocketbtn">
-        <a href="#"><img src="/assets/rocket.svg" alt="backtotop" width="24px" height="auto" /></a>
-      </button>
-    </Router>
+            
+          </Routes>
+          <Footer />
+          <button className="rocketbtn">
+            <a href="#"><img src="/assets/rocket.svg" alt="backtotop" width="24px" height="auto" /></a>
+          </button>
+        </Router>
     
   );
 };
