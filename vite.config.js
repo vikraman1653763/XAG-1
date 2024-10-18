13eda65791b233e6b570bf2c963ee17a7432cf10
vikraman1653763 <vikraman1653763@gmail.com>
2024-10-18 12:08:54 +0530

    18-10-2024

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react-swc';
 export default defineConfig({
   plugins: [react()],
   server: {
-<<<<<<< HEAD
     // https: true, 
     proxy: {
       '/api': {
@@ -13,13 +12,7 @@ export default defineConfig({
 // Target your production server
         // changeOrigin: true,
         // secure: false, 
-=======
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000', // Your backend server
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
->>>>>>> c761e88e9f6229914a1f012bc5bffa4867e74579
+
       },
     },
   },
